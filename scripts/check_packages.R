9e6470fd892f74dfc67d63d554f8e093f24f22e1
Aaron Gullickson <aarongullickson@gmail.com>
2024-02-16 14:55:56 -0800

    Add first draft of reading data chapter

--- scripts/check_packages.R
+++ scripts/check_packages.R
@@ -5,7 +5,7 @@
 #http://www.vikram-baliga.com/blog/2015/7/19/a-hassle-free-way-to-verify-that-r-packages-are-installed-and-loaded
 
 packages = c("here", "tidyverse",
-             "readr", "haven",
+             "readr", "haven", "readxl", "googlesheets4",
              "gt", "scales", "ggthemes")
 
 package.check <- lapply(packages, FUN = function(x) {
