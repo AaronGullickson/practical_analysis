1d889ef372034b47a21d6dfdef745e76f25369d5
Aaron Gullickson <aarongullickson@gmail.com>
2024-02-16 11:02:04 -0800

    Add readr and haven packages

--- scripts/check_packages.R
+++ scripts/check_packages.R
@@ -5,6 +5,7 @@
 #http://www.vikram-baliga.com/blog/2015/7/19/a-hassle-free-way-to-verify-that-r-packages-are-installed-and-loaded
 
 packages = c("here", "tidyverse",
+             "readr", "haven",
              "gt", "scales", "ggthemes")
 
 package.check <- lapply(packages, FUN = function(x) {
